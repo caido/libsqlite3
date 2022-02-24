55bfafbee7af2d16258cba488346c13893624931
Emile Fugulin <code@efugulin.com>
2022-02-24 18:24:05 -0500

    Try to include lib directory

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
+use std::env;
+
 fn main() {
     println!("cargo:rerun-if-changed=sqlite3/sha1.c");
-    cc::Build::new().file("sqlite3/sha1.c").compile("sha1");
+    cc::Build::new()
+        .include(env::var_os("DEP_SQLITE3_LIB_DIR").expect("Missing DEP_SQLITE3_LIB_DIR"))
+        .file("sqlite3/sha1.c")
+        .compile("sha1");
 }
