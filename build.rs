66154d6006067565d83f1d41e9e4e06edfd5385f
Emile Fugulin <code@efugulin.com>
2022-02-24 19:45:57 -0500

    Copy headers in library

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
-use std::env;
-
 fn main() {
-    println!("cargo:rerun-if-changed=sqlite3/sha1.c");
+    println!("cargo:rerun-if-changed=sqlite3/ext/sha1.c");
+    println!("cargo:rerun-if-changed=sqlite3/include/sqlite3.h");
+    println!("cargo:rerun-if-changed=sqlite3/include/sqlite3ext.h");
     cc::Build::new()
-        .include(env::var_os("DEP_SQLITE3_LIB_DIR").expect("Missing DEP_SQLITE3_LIB_DIR"))
-        .file("sqlite3/sha1.c")
-        .compile("sha1");
+        .include("sqlite3/include")
+        .file("sqlite3/ext/sha1.c")
+        .compile("sqlite3ext");
 }
